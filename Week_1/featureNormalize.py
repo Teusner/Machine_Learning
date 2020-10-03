b1d0a6ad1974e5ab4bd99dafd9cb148612bae3c5
Teusner <quentin.brateau@esnta-bretagne.org>
2020-10-03 14:14:15 +0200

    Gradient Descent implementing

--- Week_1/featureNormalize.py
+++ Week_1/featureNormalize.py
@@ -8,28 +8,7 @@ def featureNormalize(X):
        is 1. This is often a good preprocessing step to do when
        working with learning algorithms.
     """
-    X_norm, mu, sigma = 0,0,0
-    # ====================== YOUR CODE HERE ======================
-    # Instructions: First, for each feature dimension, compute the mean
-    #               of the feature and subtract it from the dataset,
-    #               storing the mean value in mu. Next, compute the
-    #               standard deviation of each feature and divide
-    #               each feature by it's standard deviation, storing
-    #               the standard deviation in sigma.
-    #
-    #               Note that X is a matrix where each column is a
-    #               feature and each row is an example. You need
-    #               to perform the normalization separately for
-    #               each feature.
-    #
-    # Hint: You might find the 'mean' and 'std' functions useful.
-    #
 
-
-
-
-
-
-	# ============================================================
-
+    mu, sigma = np.mean(X, axis=0), np.std(X, axis=0)
+    X_norm = (X - np.ones((X.shape))*mu) / sigma
     return X_norm, mu, sigma
